71ebd6a5d00c52a70bedfdcacd25b4716fa577e1
sunnyk56 <sunnyk@leewayhertz.com>
2021-12-06 13:08:12 +0530

    add scripts

--- deploy/ubuntu/init.sh
+++ deploy/ubuntu/init.sh
@@ -8,9 +8,9 @@ apt-get install --no-install-recommends -yq software-properties-common \
 
 echo "--------------install_golang---------------------------"
 curl https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz --output go.tar.gz
-tar -C /root -xzf go.tar.gz
-PATH="/root/go/bin:$PATH"
-GOPATH=/root/go
+tar -C $HOME -xzf go.tar.gz
+PATH="$HOME/go/bin:$PATH"
+GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
 echo "--------------install_node and npm---------------------------"
